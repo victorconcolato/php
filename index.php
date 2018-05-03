b894d5569d6e31e272222b8cb89beb8b5c1102c6
victorconcolato <38448873+victorconcolato@users.noreply.github.com>
2018-05-03 20:08:22 -0300

    Deu merda na tentativa de imitar o curso
     e aí eu carteei.

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-	echo "Olá Mundo 456!";
-=======
-	echo "Olá Mundo! 123";
+echo "Tá ligado";
 
-	echo "Olá Hcode!";
->>>>>>> 10e0e86563a7a32dfe83d94538d106fd8e626c1b
-
-	echo "Olá Udemy!";
 ?>
