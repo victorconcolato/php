1ae9460bea5df0b311e66704faef8095284e36c7
Victor Concolato <victorconcolato@gmail.com>
2018-05-03 20:18:22 -0300

    aprendendo de vdd 1

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <?php
 
-<<<<<<< HEAD
 	echo "Olá Mundo 456!";
-=======
-	echo "Olá Mundo! 123";
 
-	echo "Olá Hcode!";
->>>>>>> 10e0e86563a7a32dfe83d94538d106fd8e626c1b
-
-	echo "Olá Udemy!";
+	echo "Olá Udemy e Hcode!";
+	
 ?>
